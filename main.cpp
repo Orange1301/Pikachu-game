8932447040761956be8a88db8a1c2295c1e6765e
Nguyễn Ngọc Cảnh <130559533+Orange1301@users.noreply.github.com>
2024-03-26 21:45:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,24 +9,24 @@
 using namespace std;
 
 int main() {
-    while (true) {
-        char c = getch();
-        switch (c) {
-            case UP:
-                cout << "UP";
-                break;
-            case DOWN:
-                cout << "DOWN";
-                break;
-            case LEFT:
-                cout << "LEFT";
-                break;
-            case RIGHT:
-                cout << "RIGHT";
-                break;
-            default:
-                break;
-        }
-    }
+    // while (true) {
+    //     char c = getch();
+    //     switch (c) {
+    //         case UP:
+    //             cout << "UP";
+    //             break;
+    //         case DOWN:
+    //             cout << "DOWN";
+    //             break;
+    //         case LEFT:
+    //             cout << "LEFT";
+    //             break;
+    //         case RIGHT:
+    //             cout << "RIGHT";
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
     return 0;
 }
