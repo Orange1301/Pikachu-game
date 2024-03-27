989782227bef2397893eb61c9f647a811841546a
Nguyễn Ngọc Cảnh <130559533+Orange1301@users.noreply.github.com>
2024-03-27 14:32:36 +0700

    code menu

--- Menu.cpp
+++ Menu.cpp
@@ -9,26 +9,10 @@ void DisplayMenu() {
     
 }
 
-// void MoveMenuCursor(int direction) {
-//     // Kiểm tra xem con trỏ menu có hợp lệ hay không
-//     if (currentMenuItem >= menuItems.size()) {
-//         return;
-//     }
-
-//     // Di chuyển con trỏ menu theo hướng được chỉ định
-//     if (direction == UP) {
-//         currentMenuItem--;
-//     } else if (direction == DOWN) {
-//         currentMenuItem++;
-//     }
-
-//     // Giữ con trỏ menu trong phạm vi hợp lệ
-//     if (currentMenuItem < 0) {
-//         currentMenuItem = 0;
-//     } else if (currentMenuItem >= menuItems.size()) {
-//         currentMenuItem = menuItems.size() - 1;
-//     }
-
-//     // Hiển thị lại menu với con trỏ được cập nhật
-//     DisplayMenu();
-// }
+void MoveMenuCursor(int direc) {
+    if (direc == UP || direc = LEFT)
+        currentOption = (currentOption + 4) % 5;
+    else if (direc == DOWN || direc == RIGHT)
+        currentOption = (currentOption + 1) % 5;
+    DisplayMenu();
+}
