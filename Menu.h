7699e72f3bde40df0954b20dc5d527b5c70db1cb
Nguyễn Ngọc Cảnh <130559533+Orange1301@users.noreply.github.com>
2024-03-28 14:59:38 +0700

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,12 +1,11 @@
 #pragma once
 #include <iostream>
 #include <string>
-#include <conio.h>
 #include <unordered_map>
 #include <random>
 #include <fstream>
-#include "Controller.h"
 #include "Game.h"
+#include "Controller.h"
 
 #define NORMAL 4;
 #define HARD 8;
@@ -17,18 +16,22 @@ struct Menu
 {
 	static bool exit;									// cờ thoát game
 	static int currentOption;							// lựa chọn hiện tại
-	const static string options[5];						// tên các lựa chọn (gồm 5 lựa chọn)
+	const static string options[9];						// tên các lựa chọn
 	static void MainScreen();							// màn hình chính
-	static void DisplayMenu();							// menu
+	static void MainMenu();								// menu
+	static void PlayMenu();								// menu play
+	static void HighScores();							// màn hình load file high scores
 	static void TutorialScreen();						// màn hình hướng dẫn cách chơi
-	static void ExitScreen();							// màn hình thoát trò ch?i
+	static void ExitScreen();							// màn hình thoát trò chơi
 	static void NormalMode();							// chế độ thường
 	static void HardMode();								// chế độ khó
-	static void PrintLogo();							// in logo
+	static void DropMode();								// chế độ rơi
+	static void DisplayMenu();							// in logo
+	static void GoBack() {};							// trở về menu
 	static void PrintOptionsBoard();					// in ra bảng các lựa chọn
 	static void PrintAnimation();						// in ra hiệu ứng mở đầu
 	static void PrintRectangle(int, int, int, int);		// vẽ hình chữ nhật
-	static void MoveMenuCursor(bool, bool);				// thay đổi lựa chọn
+	static void ChangeOption(bool, bool);				// thay đổi lựa chọn
 };
 struct Player
 {
@@ -36,4 +39,3 @@ struct Player
 	int score;
 	string mode;
 };
-
