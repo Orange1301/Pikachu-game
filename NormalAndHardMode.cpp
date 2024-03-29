49d47e163550ba455c411f060764b28a4cddf8dd
Nguyễn Ngọc Cảnh <130559533+Orange1301@users.noreply.github.com>
2024-03-29 17:07:15 +0700

    ..

--- NormalAndHardMode.cpp
+++ NormalAndHardMode.cpp
@@ -1,1 +1,23 @@
 #include "NormalAndHardMode.h"
+
+Game::Game(int MODE) {
+    gameBoard
+}
+
+void Game::StartGame() {
+    system("cls");
+    RenderGameBoard();
+    RenderInfoBoard();
+
+    while (true) {
+
+    }
+}
+
+GameBoard::RenderGameBoard() {
+    
+}
+
+InfoBoard::RenderInfoBoard() {
+
+}
