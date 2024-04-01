c3df71ad7355be7c7c3c46c45313e908a5126e72
Nguyễn Ngọc Cảnh <130559533+Orange1301@users.noreply.github.com>
2024-04-01 18:31:41 +0700

    ..

--- DropMode.h
+++ DropMode.h
@@ -1,3 +1,55 @@
 #include "Menu.h"
 #include "Controller.h"
 
+struct Node {
+    char pokemon;
+    int height;
+    Node* next;
+    Node* prev;
+};
+
+struct GameBoard
+{
+    ~GameBoard();
+
+    int size;
+    int left, top;
+    int remainCells;
+
+    pair<int, int> currentCell = {0, 0};
+    pair<int, int> chosenCell1 = {-1, -1};
+    pair<int, int> chosenCell2 = {-1, -1};
+
+    char **pokemonsBoard;
+    char **background;
+
+    void Render();
+    void RenderCell(pair<int, int>, int);
+    void RemoveCell(pair<int, int>);
+};
+
+struct InfoBoard
+{
+    string playerName, mode;
+    int score = 0;
+    int lives = 3;
+    int hints = 3;
+    void Render();
+    void SaveData();
+};
+
+struct DGame
+{
+    static GameBoard gameBoard;
+    static InfoBoard infoBoard;
+
+    static void SetupGame(int);
+    static void StartGame();
+
+    static bool ExistsLine(pair<int, int>, pair<int, int>);
+    static vector<pair<int, int>> Path(vector<pair<int, int>>);
+    static vector<pair<int, int>> CheckMatching(pair<int, int>, pair<int, int>);
+
+    static void LosingScreen();
+    static void WinningScreen();
+}
